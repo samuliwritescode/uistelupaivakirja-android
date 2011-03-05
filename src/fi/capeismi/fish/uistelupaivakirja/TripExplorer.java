90641b9bcb9db9e770f0f3c26798f22baaf6c36d
Samuli Penttila <samuli.penttila@gmail.com>
2011-03-05 20:04:13 +0200

    -Alkucommit

---
+++ src/fi/capeismi/fish/uistelupaivakirja/TripExplorer.java
@@ -0,0 +1,13 @@
+package fi.capeismi.fish.uistelupaivakirja;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TripExplorer extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
