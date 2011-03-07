fce36eb102cd2e060b4412ab82a118294e28c713
Samuli Penttila <samuli.penttila@gmail.com>
2011-03-07 21:07:35 +0200

    -reissu

--- src/fi/capeismi/fish/uistelupaivakirja/TripExplorer.java
+++ src/fi/capeismi/fish/uistelupaivakirja/TripExplorer.java
@@ -1,6 +1,7 @@
 package fi.capeismi.fish.uistelupaivakirja;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.os.Environment;
@@ -8,8 +9,10 @@ import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class TripExplorer extends Activity {
+public class TripExplorer extends Activity implements OnClickListener {
 	
 	private static final String TAG = "TripExplorer";
 	
@@ -41,7 +44,8 @@ public class TripExplorer extends Activity {
         	Log.e(TAG, state);        	
         }
         setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
-      
+        Button btn = (Button)findViewById(R.id.BeginTrip);
+        btn.setOnClickListener(this);
     }
     
     @Override
@@ -56,5 +60,14 @@ public class TripExplorer extends Activity {
     	super.onResume();
     	Log.i(TAG, "resume");
     }
+
+	@Override
+	public void onClick(View arg0) {
+		Button btn = (Button)arg0;
+		Log.i(TAG, "clicked"+btn.getText().toString());
+		
+		Intent intent = new Intent(this, Trip.class);
+		startActivity(intent);
+	}
       
 }
