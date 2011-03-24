64a6bdc93b975d96fbabe97143a3f49107a81238
Samuli Penttila <samuli.penttila@gmail.com>
2011-03-24 17:50:17 +0200

    -abstrakti kantaluokka build targetille

---
+++ src/fi/capeismi/fish/uistelupaivakirja/model/AbstractBuildTarget.java
@@ -0,0 +1,33 @@
+package fi.capeismi.fish.uistelupaivakirja.model;
+
+public abstract class AbstractBuildTarget implements BuildTarget {
+	@Override
+	public void newObject(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endObject() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newKeyValue(String key, String value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void newProperty() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void endProperty() {
+		// TODO Auto-generated method stub
+		
+	}
+}
