27c9a2335aad556d1074dd53bad5b2866ae513df
Samuli Penttila <samuli.penttila@gmail.com>
2011-04-09 12:29:13 +0300

    -Kalatapahtuma ja säätapahtuma toimii

--- src/fi/capeismi/fish/uistelupaivakirja/controller/FishAndWeather.java
+++ src/fi/capeismi/fish/uistelupaivakirja/controller/FishAndWeather.java
@@ -1,5 +1,9 @@
 package fi.capeismi.fish.uistelupaivakirja.controller;
 
+import fi.capeismi.fish.uistelupaivakirja.model.EventItem;
+import fi.capeismi.fish.uistelupaivakirja.model.ModelFactory;
+import fi.capeismi.fish.uistelupaivakirja.model.TrollingObjectItem;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
@@ -10,12 +14,41 @@ public final class FishAndWeather extends Event {
     public void onCreate(Bundle savedInstanceState) {
     	setContentView(R.layout.fishnweather);
     	super.onCreate(savedInstanceState);
-    	Log.i(TAG, "new fish&weather");
+
+    	final Intent intent = getIntent();
+    	Bundle extras = intent.getExtras();
+    	int index = -1;
+    	int weatherindex = extras.getInt("tripindex");
+
+		if(!extras.containsKey("event"))
+    	{
+    		Log.i(TAG, "no previous event. create new one");
+    		TrollingObjectItem item = ModelFactory.getModel().getTrips().getList().get(weatherindex).newEvent(EventItem.EType.eFishAndWeather);
+    		index = ModelFactory.getModel().getTrips().getList().get(weatherindex).getEvents().indexOf(item);
+    	}
+		else
+		{
+			index = extras.getInt("event");
+		}
+				
+		setupFishFields(weatherindex, index);
+		readFishFields();
+		
+		setupWeatherFields(weatherindex, index);
+		readWeatherFields();
+    }
+    
+    @Override
+    public void onResume() {
+    	super.onResume();
+    	readFishFields();
+    	readWeatherFields();
     }
 
 	@Override
 	public void onDone() {
-		// TODO Auto-generated method stub
-		
+		writeFishFields();
+		writeWeatherFields();
+		getTrip().save();	
 	}
 }
