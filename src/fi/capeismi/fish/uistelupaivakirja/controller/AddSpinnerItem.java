9c6ba16e991b5a24495126b926114e95ddfbb2db
Samuli Penttila <samuli.penttila@gmail.com>
2011-04-28 21:12:09 +0300

    -Copyrightit

--- src/fi/capeismi/fish/uistelupaivakirja/controller/AddSpinnerItem.java
+++ src/fi/capeismi/fish/uistelupaivakirja/controller/AddSpinnerItem.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright 2011 Samuli Penttilä
+ *
+ * This file is part of Uistelupäiväkirja.
+ * 
+ * Uistelupäiväkirja is free software: you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free Software 
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * Uistelupäiväkirja is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Uistelupäiväkirja. If not, see http://www.gnu.org/licenses/.
+ */
+
 package fi.capeismi.fish.uistelupaivakirja.controller;
 
 import android.app.Dialog;
